7dbb5d54831326d984b168136cb5622e419a7abb
huangpiqiao <huangpiqiao@batmobi.net>
2018-07-26 17:00:48 +0800

    修改readme

--- src/components/BannerSlider.vue
+++ src/components/BannerSlider.vue
@@ -381,6 +381,36 @@
                 return this.silderItemsLenght > 1 ? -this.screenWidth : 0;
             },
         },
+        directives: {
+            imgautoshow(el,{ value }){
+                var imge = new Image();
+                //设计稿的宽高比
+                var settingRatio =  value.width / value.height;
+                imge.onload=function () {
+                    //实际图片的宽高比
+                    var  realityRatio = this.width / this.height;
+                    var imgWidth = this.width/100;
+                    var imgHeigth = this.height/100;
+                    if(realityRatio<=settingRatio){
+                        el.style.width = '100%';
+                        el.style.height = 'auto';
+                        var imgScaleHeight = imgHeigth/(imgWidth/value.width);
+                        el.style.marginTop = -(imgScaleHeight-value.height)/2+'rem';
+                    }
+                    if(realityRatio>settingRatio){
+                        el.style.height = '100%';
+                        el.style.width = 'auto';
+                        var imgScaleWidth = imgWidth/(imgHeigth/value.height);
+                        el.style.marginLeft = -(imgScaleWidth - value.width)/2+'rem';
+                    }
+                }
+                //错误则隐藏
+                imge.onerror = function () {
+                    el.style.display = 'none';
+                }
+                imge.src=el.src;
+            }
+        }
     }
 </script>
 <style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import imgAutoShow from './imgAutoShow'
-Vue.use(imgAutoShow);
+// import imgAutoShow from './imgAutoShow'
+// Vue.use(imgAutoShow);
 
 Vue.config.productionTip = false
 
